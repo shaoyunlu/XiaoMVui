d7830145fb5b6017a45f9adc9797fd83bdabd38c
shaoyunlu <shaoyunlu@126.com>
2023-04-22 23:29:33 +0800

    1

---
+++ src/components/datePicker/datePicker.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class=""></div>
+</template>
+
+<script>
+import {defineComponent} from 'vue'
+export default defineComponent({
+    name:"xmvDatePicker",
+    setup(props ,context) {
+        return {}
+    }
+})
+</script>
+
+<style lang="" scoped></style>
---
+++ src/components/datePicker/mode/datePickerMode.js
@@ -0,0 +1,5 @@
+class DatePickerMode{
+
+}
+
+export default DatePickerMode

5cd5dc55e8a419619c2f429cb9c78e0a373378cb
shaoyunlu <shaoyunlu@126.com>
2023-08-11 09:37:41 +0800

    1

--- src/components/space/space.vue
+++ src/components/space/space.vue
@@ -16,13 +16,16 @@ export default defineComponent({
         return ()=>{
             let renderSlot = []
             let defaultSlot = ( slots.default) == null ? void 0 : slots.default.call(slots)
-            defaultSlot.forEach(slot =>{
-                renderSlot.push(h('div' ,{class:'xmv-space__item',style:{
-                    'padding-bottom':size + 'px',
-                    'margin-right':size + 'px',
-                    'min-width':fill?'100%':''
-                }} ,slot))
-            })
+            if (defaultSlot[0] && defaultSlot[0].children){
+                defaultSlot[0].children.forEach(slot =>{
+                    renderSlot.push(h('div' ,{class:'xmv-space__item',style:{
+                        'padding-bottom':size + 'px',
+                        'margin-right':size + 'px',
+                        'min-width':fill?'100%':''
+                    }} ,slot))
+                })
+            }
+            
             return h('div' ,{class:'xmv-space' ,style:{
                 'flex-wrap' : wrap?'wrap':'',
                 'margin-bottom' : '-' + size + 'px',

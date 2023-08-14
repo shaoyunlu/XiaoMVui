ef2b16a24e1675a588b6aca2d5d92a7ab84c8021
shaoyunlu <shaoyunlu@126.com>
2023-08-14 10:54:45 +0800

    1

--- src/components/slider/slider.vue
+++ src/components/slider/slider.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="xmv-slider" :class="computeSliderClass"
         @click.stop="handleSliderClick" ref="sliderRef"
-        @mousedown.stop="handleSliderMousedown">
+        >
         <div class="xmv-slider__runway">
             <div class="xmv-slider__bar"></div>
-            <div class="xmv-slider__button-wrapper" :style="{left : sliderVal + '%'}">
-                <xmv-tooltip :content="sliderVal" :isAlignCenter="true" ref="tooltipRef">
+            <div class="xmv-slider__button-wrapper" 
+                :style="{left : sliderVal + '%'}"
+                @mousedown.stop="handleSliderMousedown">
+                <xmv-tooltip :content="toolTipContent" :isAlignCenter="true" ref="tooltipRef">
                     <div class="xmv-slider__button"></div>
                 </xmv-tooltip>
             </div>
@@ -14,12 +16,18 @@
 </template>
 
 <script>
-import {computed, defineComponent,onMounted,ref} from 'vue'
+import {computed, defineComponent,onMounted,ref, watch} from 'vue'
+import {isEmpty} from 'utils/data'
 export default defineComponent({
     name:"xmvSlider",
+    props:{
+        modelValue : Number,
+        formatTooltip : Function
+    },
     setup(props ,context) {
 
         const sliderVal = ref(0)
+        const toolTipContent = ref(0)
         const sliderRef = ref(null)
         const isDragging = ref(false)
         const tooltipRef = ref(null)
@@ -42,7 +50,7 @@ export default defineComponent({
             if (val >= 100)
                 val = 100
 
-            sliderVal.value = val
+            context.emit('update:modelValue' ,val)
         }
 
         const handleSliderMousedown = ()=>{
@@ -69,14 +77,34 @@ export default defineComponent({
             if (val >= 100)
                 val = 100
 
+            context.emit('update:modelValue' ,val)
+        }
+
+        const handleWatch = (val)=>{
             sliderVal.value = val
+            toolTipContent.value = formatTooltip(val)
+        }
+
+        watch(()=>props.modelValue ,(newVal)=>{
+            handleWatch(newVal)
+        })
+
+        const formatTooltip = (val)=>{
+            if (props.formatTooltip != undefined){
+                return props.formatTooltip(val)
+            }
+            return val
         }
 
         onMounted(()=>{
             sliderBound = sliderRef.value.getBoundingClientRect()
+
+            if (!isEmpty(props.modelValue)){
+                handleWatch(props.modelValue)
+            }
         })
         
-        return {sliderVal ,sliderRef , computeSliderClass, isDragging, tooltipRef,
+        return {sliderVal ,toolTipContent ,sliderRef , computeSliderClass, isDragging, tooltipRef,
                 handleSliderClick ,handleSliderMousedown ,handleWindowMousemove}
     }
 })

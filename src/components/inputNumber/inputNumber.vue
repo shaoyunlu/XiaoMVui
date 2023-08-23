53bde4739c7a590e7b8cf9feda4d109aa7b4a3f2
shaoyunlu <shaoyunlu@126.com>
2023-08-23 10:45:18 +0800

    1

--- src/components/inputNumber/inputNumber.vue
+++ src/components/inputNumber/inputNumber.vue
@@ -6,7 +6,7 @@
         <span class="xmv-input-number__increase" :class="{'is-disabled' : increaseDisabled}" @click="handleIncrease">
             <xmv-icon name="plus"></xmv-icon>
         </span>
-        <xmv-input ref="inputRef" type="number" :step="inputStep" @input="handleInput" :size="size" :disabled="isDisabled"></xmv-input>
+        <xmv-input ref="inputRef" type="number" :step="inputStep" @blur="handleBlur" @input="handleInput" :size="size" :disabled="isDisabled"></xmv-input>
     </div>
 </template>
 
@@ -19,6 +19,7 @@ export default defineComponent({
     props:{
         modelValue : [Number,String],
         step:{type:Number ,default:1},
+        stepStrictly:{type:Boolean,default:false},
         min:{type:Number},
         max:{type:Number},
         precision:{type:Number,default:0},
@@ -49,29 +50,50 @@ export default defineComponent({
         })
 
         const handleDecrease = ()=>{
+            if (isDisabled.value){
+                return false
+            }
             inputRef.value.val(props.modelValue - props.step)
         }
 
         const handleIncrease = ()=>{
+            if (isDisabled.value){
+                return false
+            }
             inputRef.value.val(props.modelValue + props.step)
         }
 
-        const handleInput = (val)=>{
-            if (isEmpty(val)){
-                context.emit('update:modelValue' ,'')
-            }else{
-                val = parseFloat(parseFloat(val).toFixed(props.precision))
-                if (val < props.min || val > props.max){
-                    if (isEmpty(props.modelValue)){
-                        val = props.min
-                    }else{
-                        val = props.modelValue
-                    }
+        const handleBlur = ()=>{
+            if (props.stepStrictly){
+                let val = inputRef.value.getVal()
+                let remainder = val%props.step
+                if (remainder != 0){
+                    val = Math.floor(val / props.step) * props.step;
                     inputRef.value.val(val)
+                    context.emit('update:modelValue' ,val)
+                    context.emit('inputChange' ,val)
                 }
-                context.emit('update:modelValue' ,val)
-                context.emit('inputChange' ,val)
             }
+        }
+
+        const handleInput = (val)=>{
+            if (isEmpty(val))
+            {
+                context.emit('update:modelValue' ,'')
+                return false
+            }
+            val = parseFloat(parseFloat(val).toFixed(props.precision))
+            if (val < props.min || val > props.max){
+                if (isEmpty(props.modelValue)){
+                    val = props.min
+                }else{
+                    val = props.modelValue
+                }
+                inputRef.value.val(val)
+            }
+            
+            context.emit('update:modelValue' ,val)
+            context.emit('inputChange' ,val)
         }
 
         const decreaseIncreaseDisabled = (val)=>{
@@ -97,7 +119,7 @@ export default defineComponent({
         })
 
         return {inputRef , inputStep ,decreaseDisabled , increaseDisabled,computeClass,isDisabled,
-                handleDecrease ,handleIncrease ,handleInput}
+                handleDecrease ,handleIncrease ,handleInput ,handleBlur}
     }
 })
 </script>

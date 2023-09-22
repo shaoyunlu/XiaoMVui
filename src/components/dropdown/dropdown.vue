9c61869481356cbc32d4a59405c9c512ff844806
shaoyunlu <shaoyunlu@126.com>
2023-09-22 15:09:33 +0800

    Release version 0.1.58

--- src/components/dropdown/dropdown.vue
+++ src/components/dropdown/dropdown.vue
@@ -2,13 +2,13 @@
 
     <div class="xmv-dropdown" v-if="splitButton">
         <xmv-button-group>
-            <xmv-button type="primary">
+            <xmv-button :type="type" :size="size">
                 <slot></slot>
             </xmv-button>
 
             <xmv-popover :trigger="trigger" pop-class="xmv-dropdown__popper" ref="popoverRef" is-align-center>
                 <template #trigger>
-                    <xmv-button type="primary">
+                    <xmv-button :type="type" :size="size">
                         <xmv-icon name="arrowDown"></xmv-icon>
                     </xmv-button>
                 </template>
@@ -36,7 +36,9 @@ export default defineComponent({
     emits:['itemClick'],
     props:{
         trigger : {type : String ,default : 'hover'},
-        splitButton : {type : Boolean ,default : false}
+        splitButton : {type : Boolean ,default : false},
+        type : {type : String ,default : 'primary'},
+        size : {type : String}
     },
     setup(props ,context) {
         const popoverRef = ref(null)

132818dc3c3b627b11f96854bfc00a033b2b7e15
shaoyunlu <shaoyunlu@126.com>
2023-09-27 10:50:43 +0800

    Release version 0.1.76

--- src/components/tabs/item.vue
+++ src/components/tabs/item.vue
@@ -1,32 +1,32 @@
 <template>
-    <div class="xmv-tabs__item" :class="computeClass" @click="handleClick" ref="itemRef">
+    <div :class="computeClass" @click="handleClick" ref="itemRef">
         {{data.label}}
     </div>
 </template>
 
 <script>
-import {computed, defineComponent ,inject, onMounted ,ref} from 'vue'
+import {computed, defineComponent ,inject, h ,ref} from 'vue'
 export default defineComponent({
     name:"",
     props:{
         data : Object,
         index : Number
     },
-    setup({data ,index} ,context) {
+    setup(props ,context) {
         const itemRef = ref(null)
         const tabsMode = inject('TabsMode')
         const computeClass = computed(()=>{
-            let res = []
+            let res = ['xmv-tabs__item']
             res.push('is-' + tabsMode.tabPosition)
-            if (data.isActive){
+            if (props.data.isActive){
                 res.push('is-active')
             }
             return res
         })
 
         tabsMode.$on('itemClick' ,(name)=>{
-            data.isActive = (name == data.name)
-            if (data.isActive){
+            props.data.isActive = (name == props.data.name)
+            if (props.data.isActive){
                 if (tabsMode.tabPosition == 'top' || tabsMode.tabPosition == 'bottom'){
                     tabsMode.barWidth.value = itemRef.value.clientWidth - 20
                     tabsMode.barTranslateX.value = itemRef.value.offsetLeft == 0 ? 0 : itemRef.value.offsetLeft + 20
@@ -38,11 +38,20 @@ export default defineComponent({
         })
 
         const handleClick = ()=>{
-            data.isActive = true
-            tabsMode.$emit('itemClick' ,data.name)
+            props.data.isActive = true
+            tabsMode.$emit('itemClick' ,props.data.name)
         }
 
-        return {computeClass ,handleClick ,itemRef}
+        return ()=>{
+            console.log(props.data.slots.label)
+            return h('div' ,{
+                'class':computeClass.value,
+                'onClick':handleClick,
+                'ref':itemRef
+            } ,props.data.slots.label?h(props.data.slots.label):props.data.label)
+        }
+
+        //return {computeClass ,handleClick ,itemRef}
     }
 })
 </script>
